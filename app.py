2ff67a7fdfa6846e98e3c559dd45fe69c4d5501f
Garrett Johnson <Garrett.Johnson@ey.com>
2023-06-15 10:13:34 -0700

    updated docker pipeline

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import time
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +7,4 @@ def index():
     return "Hi People of Earth"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
